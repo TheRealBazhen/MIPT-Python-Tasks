ba203036534713fe84dd8dd8a4c021fc61fdafd3
TheRealBazhen <andrej.bazhenov.ab1@gmail.com>
2019-04-03 22:29:20 +0300

    6th contest

---
+++ Contest6/README.md
@@ -0,0 +1,3 @@
+# Contest #5
+
+Clauses can be found [here](https://contest.yandex.ru/contest/12317/enter/).
---
+++ Contest6/broken_module.py
@@ -0,0 +1,12 @@
+def foo():
+        print(524)
+    return 1
+
+
+refrigerator)
+
+raise TypeError('Ican\'t type!')
+
+
+def bar():
+    return 2
